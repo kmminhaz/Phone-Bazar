a4ae225b07c3fbeb7885a83875590198f0c6a8aa
kmminhaz <kmminhaj.khan@gmail.com>
2022-03-02 00:18:06 +0600

    Data Loaded

---
+++ script.js
@@ -0,0 +1,38 @@
+const showResults = (data) => {
+    const showItems = document.getElementById("all-search-results");
+    showItems.innerHTML = '';
+  for (const phone of data) {
+    const itemDiv = document.createElement("div");
+    itemDiv.classList.add("col");
+    itemDiv.innerHTML = `
+        <div class="card h-100">
+            <img src="${phone.image}" class="card-img-top img-fluid" />
+            <div class="card-body">
+            <small class="text-muted fw-bold"
+                >Release Date</small
+            >
+            <h4 class="card-title fw-bold">${phone.phone_name}</h4>
+            <h5 class="card-text fw-bold">${phone.brand}</h5>
+            <button type="button" class="btn btn-primary fw-bold">Show Details</button>
+            </div>
+        </div>
+        `;
+
+    showItems.appendChild(itemDiv);
+  }
+};
+
+const searchButton = document.getElementById("search-button");
+
+searchButton.addEventListener("click", function () {
+  const searchText = document.getElementById("search-input-text");
+  const searchTextValue = searchText.value;
+
+  searchText.value = ''
+
+  fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${searchTextValue}`
+  )
+    .then((response) => response.json())
+    .then((json) => showResults(json.data));
+});
